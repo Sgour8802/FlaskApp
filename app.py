29c4b86e422721e971afe6186f9c6183d74fff0c
Sgour8802 <Sgour8802@conestogac.on.ca>
2024-06-27 01:21:47 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Serve images from the 'uploads' directory
-@app.route('/uploads/<path:filename>')
-def download_file(fil
+# # Serve images from the 'uploads' directory
+# @app.route('/uploads/<path:filename>')
+# def download_file(filename):
+#     uploads_dir = os.path.join(app.root_path, 'uploads')
+#     return send_from_directory(directory=uploads_dir, filename=filename)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+    
